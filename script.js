072d2650ae87ebc8101aa142f779d94e430ba53e
Dody Al Bejad <dodyalfayed469@gmail.com>
2025-09-30 14:11:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const tombolSapa document.getElementById('sapaButton');
-tombolSapa.addEventListener('click'function() {
+tombolSapa.addEventListener('click', function() {
   alert('Halo wok terimakasih sudah ngopi');
 });
